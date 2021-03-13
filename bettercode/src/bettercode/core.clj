256ac90a439163a84d2373c9ccf1596c9e0b170e
vision-05 <t.frankel@outlook.com>
2021-03-13 21:13:23 +0000

    added context ready for using requests

--- bettercode/src/bettercode/core.clj
+++ bettercode/src/bettercode/core.clj
@@ -2,13 +2,16 @@
   (:gen-class)
   (:require [cljfx.api :as fx]
             [clojure.string :as str]
+            [clojure.data :as data]
             [clojure.core.cache :as cache]))
 
 (def *context
   (atom 
     (fx/create-context {:title "BetterCode"
                         :file-name "TextEditor.cpp"
-                        :text-editor (slurp "/home/tim/betterCode/Server/TextEditor.cpp")}
+                        :text-editor (slurp "/home/tim/betterCode/Server/TextEditor.cpp")
+                        :action ""
+                        :argument "hehe"}
                        #(cache/lru-cache-factory % :threshold 4096))))
 
 (defn status-bar [{:keys [fx/context text-color fill-color]}]
@@ -21,22 +24,30 @@
   {:fx/type :text-area
    :text (fx/sub-val context :text-editor)
    :style {:-fx-text-fill text-color
-           :-fx-background-color fill-color}})
+           :-fx-background-color fill-color}
+   :on-text-changed {:event/type ::type-text :fx/sync true}})
+
+(defmulti handle-event :event/type)
+
+(defmethod handle-event :default [e]
+  (println e))
+
+(defmethod handle-event ::type-text [{:keys [fx/event fx/context]}]
+  {:context (fx/swap-context context assoc :text-editor event 
+                                           :argument (data/diff event (fx/sub-val context :text-editor)))})
 
 (defn -main []
   (fx/create-app *context
-    ;:event-handler handle-event
+    :event-handler handle-event
     :desc-fn (fn [_]
                 {:fx/type :stage
                  :showing true
                  :scene {:fx/type :scene
-                         :root {:fx/type :grid-pane
+                         :root {:fx/type :v-box
                          :padding 10
                          :children [{:fx/type status-bar
                                      :text-color "#E0CDCD"
-                                     :fill-color "#275F77"
-                                     :grid-pane/row 0}
+                                     :fill-color "#275F77"}
                                     {:fx/type text-edit
                                      :text-color "#000000"
-                                     :fill-color "#123F58"
-                                     :grid-pane/row 1}]}}})))
+                                     :fill-color "#123F58"}]}}})))
